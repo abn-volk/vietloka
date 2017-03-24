681498e1f22582db44589fcc96eedb007227d025
Phạm Nguyễn Hoàng <pnhoang96@gmail.com>
2017-03-24 22:51:30 +0700

    About section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,43 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Route, RouterModule }   from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+import { AboutComponent } from './about.component';
+
+
+const routes: Route[] = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     NgbModule.forRoot(),
+    RouterModule.forRoot(routes),
     BrowserModule,
     FormsModule,
     HttpModule
