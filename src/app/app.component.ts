a0d8914f95297dbd2884cea04be80e3d331194e2
Phạm Nguyễn Hoàng <pnhoang96@gmail.com>
2017-04-06 17:53:09 +0700

    Use reactive form w/ validation for login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(public fb: FormBuilder) {}
   isNavbarCollapsed = true;
+  public loginForm = this.fb.group({
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(8)]]
+  });
+
+  doLogin(event) {
+    console.log(event);
+    console.log(this.loginForm.value);
+  }
 }
