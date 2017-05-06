17adfd9298b8e6bc90ffa707419b78d7cb1d0506
Phạm Nguyễn Hoàng <pnhoang96@gmail.com>
2017-05-07 01:17:16 +0700

    Demo for House component

--- src/app/house.component.ts
+++ src/app/house.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { HouseService } from './house.service' 
+import { HouseService } from './house.service';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 
 @Component( {
   selector: 'house-section',
@@ -9,16 +10,44 @@ import { HouseService } from './house.service'
 export class HouseComponent implements OnInit, OnDestroy{
 
   constructor(private router: Router, private route: ActivatedRoute, private houseService: HouseService) {}
-
   id: string;
   sub: any;
+  house: any = {
+    image: ['http://i.imgur.com/y9VtVep.jpg', 'http://i.imgur.com/fSbhzNL.jpg'],
+    title: 'Lorem ipsum dolor sit amet lorem ipsum dolor sit amet',
+    description: 'En la mondon venis nova sento, tra la mondo iras forta voko,  per flugiloj de facila vento,  nun de loko flugu ĝi al loko.',
+    area: 30,
+    address: 'Nieuwe Binnenweg 176, Rotterdam, the Netherlands',
+    numOfTotalSlots: 9,
+    hasChildren: true,
+    hasOlders: false,
+    hasElectricHeater: true,
+    hasTV: true,
+    hasCarPark: true,
+    hasInternet: true,
+    hasWashingMachine: false,
+    WC: 'None'
+  };
+
+  host: any = {
+    id:"590b590b2043f315704f3b53",
+    name:"Vietloka",
+    picture:"https://gravatar.com/avatar/d2fa7965bdc0f8eb4e6a66426acf0574?d=identicon",
+    role:"user",
+    email:"b@e.com",
+    job: "Teacher",
+    placeOfWork: "UET-VNU"
+  }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.id = params['id'];
 
       this.houseService.getHouse(this.id).subscribe(
-        (house) => console.log(house),
+        (house) => {
+          this.house = house;
+          
+        },
         (error) => console.log(error)
       );
     })
