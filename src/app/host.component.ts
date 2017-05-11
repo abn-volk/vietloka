a4a472c58f5a50ec76338295f483b11b2251d8a6
Phạm Nguyễn Hoàng <pnhoang96@gmail.com>
2017-05-11 21:31:12 +0700

    More Rx clean-up and loading screen implementation

--- src/app/host.component.ts
+++ src/app/host.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UserService, User } from './user.service';
 import { HouseService, House } from './house.service';
+import { Observable } from 'rxjs/Rx';
+
 
 @Component( {
   selector: 'host-section',
@@ -14,6 +16,8 @@ export class HostComponent implements OnInit{
   houses: Array<House>;
   isLoggedIn = (localStorage.getItem('token') && localStorage.getItem('id'));
   isGuest = (localStorage.getItem('is_guest') === 'true');
+  notFound: boolean = false;
+  loading: boolean = true;
 
   constructor(private router: Router, private route: ActivatedRoute,
     private userService: UserService) {}
@@ -22,24 +26,21 @@ export class HostComponent implements OnInit{
     this.sub = this.route.params.subscribe(params => {
       this.id = params['id'];
 
-      // Get the host information
-      this.userService.getHost(this.id).subscribe(
-        (host) => {
-          this.host = host;
-          console.log(host);
+      // Get the host's information and houses
+      Observable.forkJoin([
+        this.userService.getHost(this.id),
+        this.userService.getAllHostHouses(this.id)
+      ]).subscribe(
+        (data) => {
+          this.host = data[0];
+          this.houses = data[1];
+          this.loading = false;
         },
-        // (error) => console.log(error)
-        (error) => window.location.replace('/home')
-      );
-
-      // Get the list of hosts' houses
-      this.userService.getAllHostHouses(this.id).subscribe(
-        (houses) => {
-          console.log(houses);
-          this.houses = houses;
-        },
-        (error) => console.log(error)
-      );
+        (error) => {
+          this.notFound = true;
+        }
+      )
+      
 
     })
   }
