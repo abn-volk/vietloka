0ff798cd894560b48c655b117f40456383fbb5fc
Vance Nguyễn <nvantien@hotmail.com>
2017-05-11 16:38:40 +0700

    Finish adding profile features

--- src/app/profile.component.ts
+++ src/app/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User, UserService } from './user.service';
 import { House, HouseService } from './house.service';
-// import { Rent, RentService} from './rent.service';
+import { Rent, RentService} from './rent.service';
 import { NgbTab, NgbTabset } from '@ng-bootstrap/ng-bootstrap';
 
 @Component( {
@@ -11,11 +11,14 @@ import { NgbTab, NgbTabset } from '@ng-bootstrap/ng-bootstrap';
 export class ProfileComponent {
   user: User;
   houses: Array<House>;
+  rents: Array<Rent>;
+  renteds: Array<Rent>;
   verified = false;
   isHost = false;
   isGuest = false;
 
-  constructor (private userService: UserService, private houseService: HouseService) {}
+  constructor (private userService: UserService, private houseService: HouseService,
+     private rentService: RentService) {}
 
   ngOnInit() {
     this.userService.getProfile()
@@ -27,7 +30,7 @@ export class ProfileComponent {
                             || localStorage.getItem('is_host') === 'true');
           this.isHost = localStorage.getItem('is_host') === 'true';
           this.isGuest = localStorage.getItem('is_guest') === 'true';
-          console.log(user)
+          // console.log(user)
         },
         reason => {
           window.location.replace('/')
@@ -35,11 +38,27 @@ export class ProfileComponent {
     this.houseService.getMyHouses().
       subscribe(
         (houses) => {
-          console.log(houses);
+          // console.log(houses);
           this.houses = houses;
         },
         (error) => console.log(error)
         // (error) => window.location.replace('/home')
+      );
+    this.rentService.getMyRents()
+      .subscribe(
+        (rents) => {
+          // console.log(rents);
+          this.rents = rents;
+        },
+        (error) => console.log(error)
+      );
+    this.rentService.getMyGuest()
+      .subscribe(
+        (renteds) => {
+          console.log(renteds);
+          this.renteds = renteds;
+        },
+        (error) => console.log(error)
       );
   }
 
